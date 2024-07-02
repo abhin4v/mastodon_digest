34c460c85de70cb6aa3787f78b36ddfcfd56f477
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2024-07-02 19:14:08 +0530

    Replaces emojis in post

--- formatters.py
+++ formatters.py
@@ -21,36 +21,38 @@ def fix_post_links(post: ScoredPost, known_instance_domains: set[str]) -> str:
     return str(soup)
 
 
+def replace_emojis(content: str, emojis: list[dict]) -> str:
+    for emoji in emojis:
+        shortcode = html.escape(emoji.shortcode)
+        content = content.replace(
+            f":{emoji.shortcode}:",
+            f'<img class="emoji" title="{shortcode}" alt="{shortcode}" src="{emoji.url}">',
+        )
+    return content
+
+
+def format_media(media: dict, media_count: int) -> str:
+    url = media.url
+    description = html.escape(media.description) if media.description != None else ""
+    caption = f"<figcaption>{description}</figcaption>" if media.description != None else ""
+    formats = {
+        "image": f'<a href="{url}"><figure><img src="{url}" alt="{description}"></img>{caption}</figure></a>',
+        "video": f'<video src="{url}" controls width="100%"></video>',
+        "gifv": f'<figure><video src="{url}" autoplay loop muted playsinline width="100%" alt="{description}"></video>{caption}</figure>',
+    }
+    if media.type in formats:
+        style = ' style="max-width: calc(50% - 5px);"' if media_count > 1 else ""
+        return f'<div class="media"{style}>{formats[media.type]}</div>'
+    else:
+        return ""
+
+
 def format_post(post: ScoredPost, mastodon_base_url: str, known_instance_domains: set[str]) -> dict:
-    def format_media(media: dict, media_count: int) -> str:
-        url = media.url
-        description = html.escape(media.description) if media.description != None else ""
-        caption = f"<figcaption>{description}</figcaption>" if media.description != None else ""
-        formats = {
-            "image": f'<a href="{url}"><figure><img src="{url}" alt="{description}"></img>{caption}</figure></a>',
-            "video": f'<video src="{url}" controls width="100%"></video>',
-            "gifv": f'<figure><video src="{url}" autoplay loop muted playsinline width="100%" alt="{description}"></video>{caption}</figure>',
-        }
-        if media.type in formats:
-            style = ' style="max-width: calc(50% - 5px);"' if media_count > 1 else ""
-            return f'<div class="media"{style}>{formats[media.type]}</div>'
-        else:
-            return ""
-
-    def format_displayname(display_name: str, emojis: dict) -> str:
-        for emoji in emojis:
-            shortcode = html.escape(emoji.shortcode)
-            display_name = display_name.replace(
-                f":{emoji.shortcode}:",
-                f'<img title="{shortcode}" alt="{shortcode}" src="{emoji.url}">',
-            )
-        return display_name
-
     account_avatar = post.account.avatar
     account_url = "https://main.elk.zone/" + post.account.url
-    display_name = format_displayname(post.account.display_name, post.account.emojis)
+    display_name = replace_emojis(post.account.display_name, post.account.emojis)
     username = post.account.username
-    content = fix_post_links(post, known_instance_domains)
+    content = replace_emojis(fix_post_links(post, known_instance_domains), post.emojis)
     media = "\n".join(
         [format_media(media, len(post.media_attachments)) for media in post.media_attachments]
     )
@@ -69,7 +71,7 @@ def format_post(post: ScoredPost, mastodon_base_url: str, known_instance_domains
         user_is_bot=post.account.bot,
         user_is_group=post.account.group,
         sensitive=post.sensitive,
-        spoiler_text=post.spoiler_text,
+        spoiler_text=replace_emojis(post.spoiler_text, post.emojis),
         content=content,
         media=media,
         is_poll="poll" in post._data and post.poll is not None,
